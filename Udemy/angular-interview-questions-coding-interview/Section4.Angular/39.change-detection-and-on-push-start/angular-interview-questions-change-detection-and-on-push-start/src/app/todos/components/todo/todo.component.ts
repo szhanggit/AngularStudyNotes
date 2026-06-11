5cdfb9c3bd2c079c330509bb6daac80cda7f785c
szhanggit <zhangyidingchina@hotmail.com>
2026-06-10 22:03:10 -0400

    Udemy

---
+++ Udemy/angular-interview-questions-coding-interview/Section4.Angular/39.change-detection-and-on-push-start/angular-interview-questions-change-detection-and-on-push-start/src/app/todos/components/todo/todo.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { TodoInterface } from 'src/app/types/todo.interface';
+
+@Component({
+  selector: 'app-todos-todo',
+  templateUrl: './todo.component.html',
+})
+export class TodoComponent {
+  @Input('todo') todoProps!: TodoInterface;
+
+  checkRender(): void{
+    console.log('checkRender');
+  }
+}
