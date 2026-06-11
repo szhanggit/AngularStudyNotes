2684a4ce543af96f5864b5ee87b65707092b4567
szhanggit <zhangyidingchina@hotmail.com>
2026-06-10 22:59:56 -0400

    sdfsdf

---
+++ txcnotes/source/txc-front-web-toc/src/assets/data/mock-data.ts
@@ -0,0 +1,67 @@
+import { Shop } from 'src/app/pages/merchant/models/shop.model';
+
+export const Shops: Shop[] = [
+    {
+        id: 0,
+        shopName: 'Test Shop Name 001',
+        identityCode: '000137114',
+        externalCode: '11222',
+        address: {
+            detailAddressLine: '2F., No. 106, Sec. 5, Xinyi Rd, Xinyi Dist.,',
+            id: 0,
+            district: 'Xinyi Dist.,',
+            cityId: 0,
+            stateOrProvinceId: 0,
+            postcode: '110022',
+            countryId: 7,
+            longitude: 120,
+            latitude: 130,
+            status: 1,
+        },
+        mainPhone: '(886) 987 654 321',
+        status: 1,
+        securityKey: '12345678998752'
+    },
+    {
+        id: 1,
+        shopName: 'Test Shop Name 002',
+        identityCode: '000137114',
+        externalCode: '11222',
+        address: {
+            detailAddressLine: '2F., No. 106, Sec. 5, Xinyi Rd, Xinyi Dist.,',
+            id: 0,
+            district: 'Xinyi Dist.,',
+            cityId: 0,
+            stateOrProvinceId: 0,
+            postcode: '110022',
+            countryId: 7,
+            longitude: 120,
+            latitude: 130,
+            status: 1,
+        },
+        mainPhone: '(886) 987 654 321',
+        status: 1,
+        securityKey: '12345678998752'
+    },
+    {
+        id: 2,
+        shopName: 'Test Shop Name 003',
+        identityCode: '000137114',
+        externalCode: '11222',
+        address: {
+            detailAddressLine: '2F., No. 106, Sec. 5, Xinyi Rd, Xinyi Dist.,',
+            id: 0,
+            district: 'Xinyi Dist.,',
+            cityId: 0,
+            stateOrProvinceId: 0,
+            postcode: '110022',
+            countryId: 7,
+            longitude: 120,
+            latitude: 130,
+            status: 1,
+        },
+        mainPhone: '(886) 987 654 321',
+        status: 1,
+        securityKey: '12345678998752'
+    }
+]
